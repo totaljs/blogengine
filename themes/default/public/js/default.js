fde2273be85550abca8c6ce28149f5f745aa76ee
Peter Širka <petersirka@gmail.com>
2016-09-05 21:50:25 +0200

    Improve code.

--- themes/default/public/js/default.js
+++ themes/default/public/js/default.js
@@ -240,8 +240,7 @@ Tangular.register('comment', function(value) {
 function smilefy(str) {
 	var db = { ':-)': 1, ':)': 1, ';)': 8, ':D': 0, '8)': 5, ':((': 7, ':(': 3, ':|': 2, ':P': 6, ':O': 4, ':*': 9, '+1': 10, '1': 11, '\/': 12 };
 	return str.replace(/(\-1|[:;8O\-)DP(|\*]|\+1){1,3}/g, function(match) {
-		var clean = match.replace('-', '');
-		var smile = db[clean];
+		var smile = db[match.replace('-', '')];
 		if (smile == null)
 			return match;
 		return '<span class="smiles smiles-' + smile + '"></span>';
