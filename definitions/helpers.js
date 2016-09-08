5bf953be8d7ca5a8b7ff3b9dc805d6f81abf0d19
Peter Širka <petersirka@gmail.com>
2016-09-08 20:38:48 +0200

    Remove `smilefy` from markdown.

--- definitions/helpers.js
+++ definitions/helpers.js
@@ -11,14 +11,5 @@ global.marked = require('marked');
 global.marked.setOptions({ gfm: true, breaks: true, sanitize: true, tables: true });
 
 global.markdown = function(text) {
-	return smilefy(marked(text)).replace(/<img/g, '<img class="img-responsive img-rounded"').replace(/<table/g, '<table class="table table-bordered"');
+	return marked(text).replace(/<img/g, '<img class="img-responsive img-rounded"').replace(/<table/g, '<table class="table table-bordered"');
 };
-
-function smilefy(str) {
-	return str.replace(SMILES_REGEXP, function(match) {
-		var smile = SMILES[match.replace('-', '')];
-		if (smile == null)
-			return match;
-		return '<span class="smiles smiles-' + smile + '"></span>';
-	});
-}
